37b40858418bc9aa840bdb6059308d4e958d160f
SkullZ1 <ronaldadithya77@gmail.com>
2022-01-18 12:47:36 +0700

    Aplikasi Desa

---
+++ app/src/main/java/com/example/aplikasidesa/UpdateAccount.java
@@ -0,0 +1,42 @@
+package com.example.aplikasidesa;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UpdateAccount extends AppCompatActivity {
+    private SharedPreferences mPreferences;
+    private String mSharedPrefFile = "account";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_account);
+        TextView result = findViewById(R.id.greetings);
+
+        mPreferences = getSharedPreferences(mSharedPrefFile,
+                MODE_PRIVATE);
+
+        result.setText(mPreferences.getString("username",null));
+    }
+
+    public void logout(View view) {
+        Preferences.clearLoggedInUser(getBaseContext());
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+    @Override
+
+    public void onBackPressed() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+
+
+}
